828405d6b82cfbd03564548bfa82e58d6e5bad60
Samantha <samareimorgan@gmail.com>
2018-12-15 14:30:28 -0700

    add medication api

---
+++ hotsheetmedical/routes/api/medication.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const medicationController = require("../../controllers/medicationcontroller");
+
+//Matches api/medication
+router.route("/")
+    .get(medicationController.findAll)
+    .post(medicationController.create);
+
+//Matches with api/medication/:id
+
+router
+    .route("/:id")
+    .get(medicationController.findById)
+    .put(medicationController.update)
+    .delete(medicationController.remove);
+
+module.exports = router;
+
