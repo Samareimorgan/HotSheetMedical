34fcec7e47cbc16091e227168b0c5bae63af154a
Samantha <samareimorgan@gmail.com>
2018-12-15 18:14:31 -0700

    Add allergies api routes

---
+++ hotsheetmedical/routes/api/allergies.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const allergiescontroller = require("../../controllers/allergiescontroller");
+
+//Matches /api/allergies
+router.route("/")
+    .get(allergiescontroller.findAll)
+    .post(allergiescontroller.create);
+
+//Matches /api/allergies/:id
+router
+    .route("/:id")
+    .get(allergiescontroller.findById)
+    .put(allergiescontroller.update)
+    .delete(allergiescontroller.remove);
+
+module.exports = router;
