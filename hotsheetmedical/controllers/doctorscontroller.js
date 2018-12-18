eac42ceb8b771202940a1814b48cee643a55d4d7
Samantha <samareimorgan@gmail.com>
2018-12-18 12:01:44 -0700

    Add Doctors Controller

---
+++ hotsheetmedical/controllers/doctorscontroller.js
@@ -0,0 +1,43 @@
+const db = require("../controllers")
+
+//defining methods for the doctors controller
+
+module.exports = {
+        findAll: function(req,res) {
+            db.Doctors
+                .find(req.query)
+                .sort({date:-1})
+                .then(dbModel => res.json(dbModel))
+                .catch(err =>res.status(422).json(dbModel))
+        }, 
+
+        findById: function(req,res){
+            db.Doctors
+                .find(req.params.id)
+                .then(dbModel => res.json(dbModel))
+                .catch(err =>res.status(422).json(dbModel))
+        },
+
+        create: function(req,res) {
+            db.Doctors
+                .create(req.body)
+                .then(dbModel =>res.json(dbModel))
+                .catch(err => res.status(422).json(dbModel))
+        }, 
+
+        update: function(req,res) {
+            db.Doctors  
+                .findOneAndUpdate({_id:req.params.id}, req.body)
+                .then(dbModel => res.json(dbModel))
+                .catch(err => res.status(422).json(dbModel))
+        },
+
+        remove: function(req,res) {
+            db.Doctors
+                .findById({_id:req.params.id})
+                .then(dbModel => dbModel.remove())
+                .then(dbModel => res.json(dbModel))
+                .catch(err =>res.status(422).json(dbModel))
+        }
+
+};
--- hotsheetmedical/controllers/insurancecontroller.js
+++ hotsheetmedical/controllers/insurancecontroller.js
@@ -3,36 +3,36 @@ const db = require("../controllers")
 //defining the methods for insurance db
 module.exports = {
     findAll: function(req,res) {
-        db.Insurance
-        .find(req.query)
-        .sort({date: -1})
-        .then(dbModel => res.json(dbModel))
-        .catch(err =>res.status(422).json(err))
+        db.Insurances
+            .find(req.query)
+            .sort({date: -1})
+            .then(dbModel => res.json(dbModel))
+            .catch(err =>res.status(422).json(err))
     }, 
 
     findById: function(req,res) {
-        db.Insurance
+        db.Insurances
             .findById(req.params.id)
             .then(dbModel => res.json(dbModel))
             .catch(err =>res.status(422).json(err))
     },
 
     create: function(req,res) {
-        db.Insurance
+        db.Insurances
             .create(req.body)
             .then(dbModel =>res.json(dbModel))
             .catch(err =>res.status(422).json(err))
     },
 
     update: function(req,res) {
-        db.Insurance
+        db.Insurances
             .findOneAndUpdate({ _id: req.params.id}, req.body)
             .then(dbModel =>res.json(dbModel))
             .catch(err =>res.status(422).json(err))
     },
 
     remove: function(req,res) {
-        db.Insurance
+        db.Insurances
             .findbyId({_id: req.params.id})
             .then(dbModel => dbModel.remove())
             .then(dbModel =res.json(dbModel))
