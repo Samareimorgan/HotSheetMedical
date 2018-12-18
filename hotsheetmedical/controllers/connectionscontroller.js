c2380a240f132e305a9925bab75d2dc8fc48b664
Samantha <samareimorgan@gmail.com>
2018-12-18 12:13:04 -0700

    Add connections controller

---
+++ hotsheetmedical/controllers/connectionscontroller.js
@@ -0,0 +1,41 @@
+const db = require("../controllers")
+
+//define methods for the connections controller
+module.exports = {
+    findAll: function(req,res) {
+        db.Connections
+            .find(req.query)
+            .then(dbModel =>res.json(dbModel))
+            .catch(err => res.status(422).json(dbModel))
+    },
+
+    findById: function(req,res) {
+        db.Connections
+            .findbyId(req.params.id)
+            .then(dbModel =>res.json(dbModel))
+            .catch(err => res.status(422).json(dbModel))
+    },
+
+    create: function(req,res){
+        db.Connections
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(dbModel))
+    },
+
+    update: function(req,res) {
+        db.Connections
+        .findOneAndUpdate({_id:req.params.id},req.body)
+        .then(dbModel =>res.json(dbModel))
+        .catch(err => res.status(422).json(dbModel))
+    },
+
+    remove: function(req,res) {
+        db.Connections
+        .findById({_id:req.params.id})
+        .then(dbModel => dbModel.remove())
+        .then(dbModel =>res.json(dbModel))
+        .catch(err => res.status(422).json(dbModel))
+
+    }
+};
