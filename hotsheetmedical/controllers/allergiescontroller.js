4f5c4f079fbb345583588c2ac80db032fd692fd9
Samantha <samareimorgan@gmail.com>
2018-12-18 12:45:07 -0700

    Add Allergies Controller

--- hotsheetmedical/controllers/allergiescontroller.js
+++ hotsheetmedical/controllers/allergiescontroller.js
@@ -8,5 +8,34 @@ module.exports = {
             .find(req.query)
             .then(dbModel =>res.json(dbModel))
             .catch(err =>res.status(422).json(err))
+    },
+
+    findById: function(req,res){
+        db.Allergies
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err =>res.status(422).json(err))
+    },
+
+    create: function(req,res) {
+        db.Allergies
+            .create(req.body)
+            .then(dbModel=>res.json(dbModel))
+            .catch(err =>res.status(422).json(err))
+    },
+
+    update: function(req,res) {
+        db.Allergies
+            .findOneAndUpdate({_id:req.params.id},req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err =>res.status(422).json(err))
+    },
+    
+    remove: function(req,res) {
+        db.Allergies
+            .findById({_id:req.params.id})
+            .then(dbModel=>dbModel.remove())
+            .then(dbModel =>res.json(dbModel))
+            .catch(err =>res.status(422).json(err))
     }
-}
+};
