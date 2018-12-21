c8d0736dab74ee2745602a4459c02b0e957be5a7
Samantha <samareimorgan@gmail.com>
2018-12-20 20:35:21 -0700

    Add Doctors Model

---
+++ hotsheetmedical/models/doctors.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const docSchema = new Schema ({
+    doctor_id:{
+        type: ObjectId, 
+        required: true
+    },
+    doctor_type: {
+        type: String, 
+        required: true
+    },
+    doctor_info: {
+        doc_first_name: 
+        {
+            type: String, 
+            required: true
+        },
+
+        doc_last_name: 
+        {
+            type: String, 
+            required: true
+        }
+
+    },
+    doctor_location: {
+        office_name:
+        {
+            type: String,
+            required: true
+        },
+        office_street_address: {
+            type: String,
+            required: true
+        },
+        office_city: {
+            type: String, 
+            required: true
+        },
+        office_state: {
+            type: String, 
+        },
+        office_zip_code: {
+            type: Integer,
+            required: true,
+            validate: input => {
+                if (input.length !== 5) {
+                    "Please enter the 5 digits for the zip code"
+                }
+            }
+        }, 
+    },
+    doctor_phone_number: {
+        type: Integer,
+        required: true, 
+        validate: input => {
+            if (input.length !== 10) {
+                "Please enter the 10 digits for the doctor's phone number"
+            }
+        }
+    }
+    
+});
+
+const Doctors = ("Doctors", docSchema);
+
+module.exports = Doctors;
