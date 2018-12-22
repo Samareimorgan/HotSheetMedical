3c75e2dc021361e0e7aaeaf5a50e3d9e3affb9c9
Samantha <samareimorgan@gmail.com>
2018-12-22 01:34:49 -0700

    Add index routes & update server.js

--- hotsheetmedical/server.js
+++ hotsheetmedical/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("/routes");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === "production") {
 app.use(routes);
 
 //Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb:/localhost/hotsheetmedical");
+mongoose.connect(process.env.MONGODB_URI || "mongodb:/localhost:27017/hotsheetmedical", { useNewUrlParser: true });
 
 //Start the API Server
 app.listen(PORT, function() {
