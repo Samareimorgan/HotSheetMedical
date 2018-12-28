c17e2673bcdda3630db1510e3b23efecff59dd39
Samantha <samareimorgan@gmail.com>
2018-12-28 13:48:18 -0700

    Update HomePage

---
+++ hotsheetmedical/models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Allergies: require("./Allergies"),
+    Connections: require("./Connections"),
+    Doctors: require("./Doctors"),
+    Insurances: require("./Insurances"),
+    Medications: require("./Medication"),
+    Users: require("./Users")
+};
