090ba9f0cb592218c0b03730badee628bc17f16f
Samantha <samareimorgan@gmail.com>
2019-01-12 12:44:51 -0700

    Finalize MAPI get call

--- client/src/Containers/UserMedication/UserMedication.js
+++ client/src/Containers/UserMedication/UserMedication.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import unirest from "unirest";
 import classes from "./UserMedication.css";
-
+import Modal from "../../components/Modal/Modal";
+console.log(process.env);
 
 class UserMedication extends Component {
     constructor (props) {
@@ -13,46 +14,65 @@ class UserMedication extends Component {
         this.medicationInputHandler = this.medicationInputHandler.bind(this);
 
     }
+
+    saveMed = (info) => {
+        unirest.post("/api/users/save", info);
+    }
+
+
     
 
     medicationInputHandler = (event) => {
-        this.setState({value:event.target.value});
-            console.log(this.state.value);
-    }
+        
     
-    componentDidMount() {
-        let query = this.state.value;
-    // const API_KEY = process.env.REACT_APP_MAPI_API_KEY;
-    // console.log({API_KEY});
-        unirest.get("https://iterar-mapi-us.p.mashape.com/api/autocomplete?query=" + {query} + "")
-        .header("X-Mashape-Key", "")
-        .header("Accept", "application/json")
-        .end(result => {
-            console.log(result.body);
-                    const response = result.body
-                    this.setState({suggestions: response.suggestions});
+        let query = event.target.value;
+        
+        const API_KEY = process.env.REACT_APP_MAPI_API_KEY;
+      
+        
+        if(query.length === 3) {
+            
+            console.log("Triggered");
+            unirest.get("https://iterar-mapi-us.p.mashape.com/api/autocomplete?query=" + query)
+            .header("X-Mashape-Key", API_KEY)
+            .header("Accept", "application/json")
+            .end(result => {
+                console.log(result.body);
+                        const response = result.body
+                        this.setState({suggestions: response.suggestions, value: query});
         });
 
+        } else {
+            this.setState({value:event.target.value})
+        }
+        
+    
     }
+    
     
     render () 
     {
-        
-        const suggestions = this.state.suggestions.map((suggestion) => {
+        let suggestions;
+        if(this.state.suggestions) {
+            suggestions = this.state.suggestions.map((suggestion) => {
             return (
                <option key = {suggestion}>
                 {suggestion}
                </option>
             )
-            
         })
+    }
         return(
             
             <div className = {classes.Medication}>
+             <Modal>
                 <input type = "text" value={this.state.value} onChange = {this.medicationInputHandler} placeholder="Input the first 3 letters of your medication" ></input>
                 <select className = {classes.Suggestions}>
                     {suggestions}
                 </select>
+                
+                <button type = "submit" className={classes.MedicationSave}>SAVE</button>
+                </Modal>
             </div>
         );
 
