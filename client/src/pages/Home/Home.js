557c2aaefdef1c6c50e5def0ef545e5ffe84df90
Samantha <samareimorgan@gmail.com>
2019-01-18 21:17:57 -0700

    Update Home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,16 +2,15 @@ import React, {Component} from "react";
 //import API from "../../utils/API";
 import EmergencyButton from "../../components/EmergencyButton/EmergencyButton";
 import Layout from "../../components/Layout/Layout";
-
+import {Link} from "react-router-dom";
 import classes from "./home.css"
 
 class Home extends Component {
   
 
-
 render() {
     return (
-    <div>
+    <div> 
         <Layout>
      
         <div className ={classes.EmergencyHomeTitle}>
@@ -20,11 +19,11 @@ render() {
      
             <div className ={classes.HomeEmergencyBtns}> 
 
-                <EmergencyButton name = " Samantha" />
-                <EmergencyButton />
-                <EmergencyButton />
-                <EmergencyButton />
-                <EmergencyButton />
+                <EmergencyButton name =" Samantha Johnston" link="/QRCode"/>
+                {/* <EmergencyButton name = "Different Patient" />
+                <EmergencyButton name = "Different Patient" />
+                <EmergencyButton name = "Different Patient" />
+                <EmergencyButton name = "Different Patient" /> */}
             
             </div>
         </Layout>
