856bef8ce299173ceaeb3c8a288b6f5f8c9088ae
Samantha <samareimorgan@gmail.com>
2019-01-18 20:57:21 -0700

    Update HotSheetButton js

--- client/src/components/HotSheetButton/HotSheetButton.js
+++ client/src/components/HotSheetButton/HotSheetButton.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+import classes from "./HotSheetButton.css";
+
+const hotSheetButton = props => {
+return(
+    <div >
+        <button className ={classes.HotSheetBtn}>Go To HotSheet  <i class="fas fa-fire"></i> </button>
+    </div>
+)
+}
+
+export default hotSheetButton;
