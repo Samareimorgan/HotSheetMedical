ae09cbb6c2285ac47ce346d9aea99400a97dba65
Samantha <samareimorgan@gmail.com>
2019-01-19 05:27:58 -0700

    Update LoginOrRegister Path

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom
 import Home from "./pages/Home/Home.js";
 import UserProfile from "./pages/UserProfile/UserProfile";
 import DatabaseEdit from "./pages/DatabaseEdit/DatabaseEdit";
-import LoginOrRegister from "./pages/LoginOrRegister/LoginOrRegister";
+// import LoginOrRegister from "./pages/LoginOrRegister/LoginOrRegister";
 import Qrcode from "./pages/QRCode/QRCode";
 
 import LoginModal from "../src/components/LogIn/LoginModal/LoginModal";
@@ -95,7 +95,7 @@ const app = props => (
         
             <Route  path = "/UserProfile" component = {UserProfile} />
         
-            <Route  path = "/LoginOrRegister" component = {LoginOrRegister} />
+            {/* <Route  path = "/LoginOrRegister" component = {LoginOrRegister} /> */}
 
             <Route  path = "/QRCode" component = {Qrcode} />
 
