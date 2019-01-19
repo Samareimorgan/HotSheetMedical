d39969127e3d0f590e9f8f99a5691694ba8793c1
Samantha <samareimorgan@gmail.com>
2019-01-18 21:09:31 -0700

    Add HotSheet Page

---
+++ client/src/pages/HotSheet/HotSheet.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import HotSheetData from "../../components/HotSheetData/HotSheetData";
+import Auxiliary from "../../hoc/Auxiliary";
+
+
+class HotSheet extends Component {
+
+    render() {
+        return(
+
+            <HotSheetData />
+
+        )
+    }
+}
+
+export default HotSheet;
