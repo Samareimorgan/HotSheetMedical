9656f0b3bc85d9040eb6649bf71c8fe507c0700d
Samantha <samareimorgan@gmail.com>
2019-01-19 19:56:18 -0700

    update

--- controllers/doctorscontroller.js
+++ controllers/doctorscontroller.js
@@ -3,9 +3,9 @@ const db = require("../models");
 //defining methods for the doctors controller
 
 module.exports = {
-        findAll: function(req,res) {
-            db.Doctors
-                .find(req.query)
+        // findAll: function(req,res) {
+        //     db.Doctors
+        //         .find(req.query)
                 .sort({date:-1})
                 .then(dbModel => res.json(dbModel))
                 .catch(err =>res.status(422).json(err))
